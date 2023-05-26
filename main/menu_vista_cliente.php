4ec41c3540a566801b619761a415c041cef1d0ba
Antoniobox <antonioboxsanchez@gmail.com>
2023-05-26 10:52:40 +0200

    Añadido html para menu_vista_cliente.php

---
+++ main/menu_vista_cliente.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang=”es”>
+<head>
+	<meta charset=”UTF-8”>
+	<title>Datos del cliente</title>
+</head>
+<body>
+	<h1>Datos del cliente</h1>
+	<form method=”post” action=”menu_vista_cliente.php”>
+		<label for=”dni”>DNI</label>
+		<input type=”text” name=”dni”>
+<label for=”nombre”>DNI</label>
+		<input type=”text” name=”nombre”>
+		<label for=”apellido1”>DNI</label>
+		<input type=”text” name=”apellido1”>
+		<label for=”apellido2”>DNI</label>
+		<input type=”text” name=”apellido2”>
+		<label for=”correo”>DNI</label>
+		<input type=”text” name=”correo”>
+		<label for=”direccion”>DNI</label>
+		<input type=”text” name=”direccion”>
+		<label for=”telefono”>DNI</label>
+		<input type=”text” name=”telefono”>
+		<label for=”codigo_acceso”>DNI</label>
+		<input type=”text” name=”codigo_acceso”>
+
+	</form>
+</body>
+</html>
