a7dbaa22b072d1b6cec6c8ff904381d7d5f27af3
Jorge <jocargi04@gmail.com>
2023-05-26 11:11:04 +0200

    correcion nombre labels

--- main/menu_vista_cliente.php
+++ main/menu_vista_cliente.php
@@ -9,21 +9,25 @@
 	<form method=”post” action=”menu_vista_cliente.php”>
 		<label for=”dni”>DNI</label>
 		<input type=”text” name=”dni”>
-<label for=”nombre”>DNI</label>
+<label for=”nombre”>Nombre</label>
 		<input type=”text” name=”nombre”>
-		<label for=”apellido1”>DNI</label>
+		<label for=”apellido1”>Apellido 1</label>
 		<input type=”text” name=”apellido1”>
-		<label for=”apellido2”>DNI</label>
+		<label for=”apellido2”>Apellido 2</label>
 		<input type=”text” name=”apellido2”>
-		<label for=”correo”>DNI</label>
-		<input type=”text” name=”correo”>
+		<label for=”correo”>Correo</label>
+		<input type=”email” name=”correo”>
 		<label for=”direccion”>DNI</label>
 		<input type=”text” name=”direccion”>
 		<label for=”telefono”>DNI</label>
-		<input type=”text” name=”telefono”>
+		<input type=”tel” name=”telefono”>
 		<label for=”codigo_acceso”>DNI</label>
 		<input type=”text” name=”codigo_acceso”>
+
+
 
 	</form>
 </body>
 </html>
+
+
