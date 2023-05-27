c09ac8f6e76d87f5d816a8c7c2e327da57579d88
Jorge <jocargi04@gmail.com>
2023-05-27 13:04:08 +0200

    Agregacion tabla usuario_cliente

--- dbTarifaDreams.sql
+++ dbTarifaDreams.sql
@@ -7,7 +7,14 @@ dni CHAR(9) PRIMARY KEY,
 nombre VARCHAR(30) NULL,
 apellido_1 VARCHAR(30) NULL,
 apellido_2 VARCHAR(30) NULL,
-correo VARCHAR(50) NOT NULL UNIQUE,
+CREATE DATABASE tarifadreams CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+CREATE TABLE IF NOT EXISTS cliente(
+dni CHAR(9) PRIMARY KEY,
+nombre VARCHAR(30) NULL,
+apellido_1 VARCHAR(30) NULL,
+apellido_2 VARCHAR(30) NULL,
+correo VARCHAR(50) NOT NULL,
 direccion VARCHAR(60) NULL,
 localidad VARCHAR(25) NULL,
 telefono CHAR(13) NULL,
@@ -35,9 +42,18 @@ CONSTRAINT FK_reserva_habitacion FOREIGN KEY (id_habitacion) REFERENCES habitaci
 
 CREATE TABLE IF NOT EXISTS usuario(
 nombre VARCHAR(30) PRIMARY KEY COMMENT 'El nombre hace referencia a lo que seria el nombre de usuario, no el nombre de la persona',
-correo VARCHAR(50) NOT NULL UNIQUE,
+correo VARCHAR(50) NOT NULL,
 contrasena VARCHAR(25) NOT NULL,
-rol BOOLEAN NOT NULL COMMENT '0. Usuario corriente | 1. Administrador',
-id_cliente CHAR(9) NOT NULL,
-CONSTRAINT FK_usuario_cliente FOREIGN KEY (id_cliente) REFERENCES cliente(dni) ON UPDATE CASCADE ON DELETE CASCADE
+rol BOOLEAN NOT NULL COMMENT '0. Usuario corriente | 1. Administrador'
+)ENGINE=INNODB;
+
+CREATE TABLE usuario_cliente(
+id_cliente CHAR(9),
+id_usuario VARCHAR(30),
+PRIMARY KEY(
+id_cliente,
+id_usuario
+),
+CONSTRAINT FK_usuario_cliente FOREIGN KEY (id_cliente) REFERENCES cliente(dni) ON UPDATE CASCADE ON DELETE RESTRICT,
+CONSTRAINT FK_cliente_usuario FOREIGN KEY (id_usuario) REFERENCES usuario(nombre) ON UPDATE CASCADE ON DELETE RESTRICT
 )ENGINE=INNODB;
