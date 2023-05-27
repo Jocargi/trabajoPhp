63d097af7d80a198aac8e0ec932a1027c84fcb01
Antoniobox <antonioboxsanchez@gmail.com>
2023-05-27 11:23:19 +0200

    Header terminado

---
+++ main/vistaReservasCliente.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Mis reservas</title>
+    <link rel="stylesheet" href="vistaReservasCliente.css">
+</head>
+<body>
+    <header>
+        <h1>Mis reservas</h1>
+    </header>
+    <main>
+        
+    </main>
+</body>
+</html>
