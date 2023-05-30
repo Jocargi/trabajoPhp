6dcf8f535284c21019d7819ea06cab35a183769b
Antoniobox <antonioboxsanchez@gmail.com>
2023-05-30 18:41:45 +0200

    Añadida la lógica al script

--- main/crearReserva.php
+++ main/crearReserva.php
@@ -1,3 +1,26 @@
+<?php
+if(isset($_POST) && !empty(!$POST['fecha-entrada']) && !empty($_POST['fecha-salida'])
+&& !empty($_POST['precio']) && !empty($_POST['max_personas'])){
+    require_once('dbaccess.php');
+    $fecha_entrada = $_POST['fecha-entrada'];
+    $fecha_salida = $_POST['fecha-salida'];
+    $precio = $_POST['precio'];
+    $numPersonas = $_POST['max_personas'];
+    $sql = "SELECT * 
+            FROM habitacion
+            WHERE id NOT IN (
+                SELECT r.id_habitacion
+                FROM reserva r
+                WHERE (r.fecha_entrada <= '$fecha_entrada' AND r.fecha_salida >= '$fecha_entrada')
+                    OR (r.fecha_entrada <= '$fecha_salida' AND r.fecha_salida >= '$fecha_salida')
+                    OR (r.fecha_entrada >= '$fecha_entrada' AND r.fecha_salida <= '$fecha_salida')
+            ) AND precio <= $precio AND max_personas >= $numPersonas";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+}
+
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -11,24 +34,23 @@
     <header>
     <h1>Tarifa dreams</h1>
     <div class="name-banner">
-        <form action="crearReservaLogica.php" method="post">
-            <input type="number" name="personas" value="<?php echo $_POST['personas']; ?>" placeholder="Nº de personas">
+        <form action="crearReserva.php" method="post">
+            <input type="number" name="max_personas" value="<?php echo $_POST['personas']; ?>" placeholder="Nº de personas">
             <input type="date" name="fecha-entrada" value="<?php echo $_POST['fecha-entrada']; ?>" >
             <input type="date" name="fecha-salida" value="<?php echo $_POST['fecha-salida']; ?>" >
             <input type="number" name="precio" value="<?php echo $_POST['precio']; ?>" placeholder="Precio">
+            <input type="submit" value="Buscar">
         </form>
     </div>
     </header>
     <main>
-        <div class="habitacion">
-            
-        </div>
-        <div class="habitacion">
-
-        </div>
-        <div class="habitacion">
-
-        </div>
+        <?php
+        while($habitaciones = $stmt->fetch()){
+            echo '<div class="habitacion">';
+            echo 'habitacion';
+            echo '</div>';
+        }    
+        ?>
     </main>
 </body>
 </html>
