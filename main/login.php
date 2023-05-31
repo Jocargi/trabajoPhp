baf7b9649ee449da42283a630df2d56e72fec04e
SeguraJo <114017348+SeguraJo@users.noreply.github.com>
2023-05-31 03:50:47 +0200

    Mensaje de login incorrecto implementado

--- main/login.php
+++ main/login.php
@@ -1,5 +1,63 @@
+<?php
+
+require_once "dbaccess.php";
+
+session_start();
+
+$email = isset($_POST["email"]) ? $_POST["email"] : null;
+$password = isset($_POST["password"]) ? $_POST["password"] : null;
+$enviarPresed = isset($_POST['enviar']);
+
+if ($enviarPresed) {
+
+    $mensaje = false;
+
+    $sql = "SELECT correo, contrasena, rol, id_cliente FROM usuario WHERE correo LIKE :email AND contrasena LIKE :password";
+
+    $array_values = [];
+
+    $array_values[":email"] = $email;
+    $array_values[":password"] = $password;
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute($array_values);
+
+    $resultado = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+    $resultado = $stmt->fetch();
+
+    if ($resultado) $mensaje = true;
+
+    if (!empty($resultado)) {
+
+        if (($resultado['rol'])) {
+            $_SESSION['correo'] = $resultado['correo'];
+            header("Location: vistaAdministradorCliente.php");
+        } else {
+
+            $sql_cliente = "SELECT * FROM CLIENTE WHERE dni like :dni";
+
+            $stmt = $pdo->prepare($sql_cliente);
+            $stmt->execute(array(':dni' => $resultado['id_cliente']));
+
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+            $resultado = $stmt->fetch();
+
+            $_SESSION['id'] = $resultado['dni'];
+            $_SESSION['nombre'] = $resultado['nombre'];
+
+            header("Location: menu_clientes.php");
+        }
+    }
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -17,13 +75,12 @@
                     <img src="images/user-6769.svg" alt="">
                 </div>
                 <form action="login.php" method="POST">
-                    <input type="email" class="input-login" id="email" name="email" placeholder="Nombre de usuario o correo electronico">
+                    <div class="mensajeError" style="<?php echo $mensaje ?? "display:none;"; ?>">
+                        <h3>Correo o contraseña incorrectos</h3>
+                    </div>
+                    <input type="email" class="input-login" id="email" name="email" value="<?php echo isset($_POST['email']) ? $email : ""; ?>" placeholder="Nombre de usuario o correo electronico">
                     <input type="password" id="password" class="input-login" name="password" placeholder="Contraseña" />
                     <input type="submit" name="enviar" class="submit" value="INICIAR SESION">
-                    <?php
-                    include "dbaccess.php";
-                    include "loginLogica.php";
-                    ?>
                     <div class="no-registrado">
                         <a href="vistaformularioRegistro.php">¿Aun no te has registrado?</a>
                     </div>
