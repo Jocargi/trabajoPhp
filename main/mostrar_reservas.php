8bd83334c126c2f191707e7678994c772b40edde
Antoniobox <antonioboxsanchez@gmail.com>
2023-06-05 10:36:11 +0200

    Lógica y tablas añadidas

--- main/mostrar_reservas.php
+++ main/mostrar_reservas.php
@@ -3,6 +3,18 @@ require_once "../main/auth_inc.php";
 require_once "../main/dbaccess.php";
 
 $correo = $_SESSION['correo'];
+
+$sql = "SELECT r.num_personas, r.fecha_entrada, r.fecha_salida
+        FROM reserva r JOIN cliente c ON c.dni=r.id_cliente
+        WHERE c.correo LIKE :correo";
+
+$stmt = $pdo->prepare($sql);
+
+$arrCorreo = [
+    ':correo' => $correo
+];
+
+$stmt->execute($arrCorreo);
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -23,6 +35,15 @@ $correo = $_SESSION['correo'];
                 <th>Fecha entrada</th>
                 <th>Fecha salida</th>
             </thead>
+            <?php
+            while($resultado = $stmt->fetch()){
+                echo '<tr>';
+                echo '<td>'. $resultado['num_personas']. '</td>';
+                echo '<td>'. $resultado['fecha_entrada']. '</td>';
+                echo '<td>'. $resultado['fecha_salida']. '</td>';
+                echo '</tr>';
+            }
+            ?>
         </table>
     </main>
 </body>
